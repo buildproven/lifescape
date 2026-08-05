04fe6aecce4f45e1e0c2d63b70d4a824ccc5b4dc
Brett Stark <brett@brettstark.com>
2026-08-05 08:22:46 -0500

    chore: remove brittle help width override

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,7 +11,6 @@ from typer.testing import CliRunner
 from lifescape.cli import app
 
 runner = CliRunner()
-runner.env["COLUMNS"] = "200"
 
 CENSUS_PAYLOAD: list[list[str]] = [
     ["NAME", "DP02_0068PE", "state", "place"],
